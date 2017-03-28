cfa50ef286249506164015bcb6bbd8deff644f95
kronosengineer2 <kronosengineer2@gmail.com>
2017-03-27 23:37:02 -0500

    This is Sping JDBC example

---
+++ SPRINGJDBCTEMPLATE/src/main/java/com/peddi/learning/dao/ContactDAO.java
@@ -0,0 +1,21 @@
+package com.peddi.learning.dao;
+
+import java.util.List;
+
+import com.peddi.learning.model.Contact;
+
+/**
+ * Defines DAO operations for the contact model.
+ * @author www.codejava.net
+ *
+ */
+public interface ContactDAO {
+	
+	public void saveOrUpdate(Contact contact);
+	
+	public void delete(int contactId);
+	
+	public Contact get(int contactId);
+	
+	public List<Contact> list();
+}
